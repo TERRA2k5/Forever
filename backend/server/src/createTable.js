c69e7a6343cc35ac3c1d96f2e4e3720744ed761e
aryan <aryananand005@gmail.com>
2025-07-24 19:59:21 +0530

    render

--- backend/server/src/createTable.js
+++ backend/server/src/createTable.js
@@ -3,7 +3,7 @@ import pool from './db.js';
 const createUserTable = async () => {
     const query = `
     CREATE TABLE IF NOT EXISTS user_positions (
-      id VARCHAR(100) NOT NULL,
+      id VARCHAR(100) PRIMARY KEY,
       latitude FLOAT,
       longitude FLOAT
     );
