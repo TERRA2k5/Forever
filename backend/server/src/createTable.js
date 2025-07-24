90ec6324e175293ea172c167754997008b20640a
aryan <aryananand005@gmail.com>
2025-07-24 19:56:02 +0530

    render

--- backend/server/src/createTable.js
+++ backend/server/src/createTable.js
@@ -2,17 +2,16 @@ import pool from './db.js';
 
 const createUserTable = async () => {
     const query = `
-    CREATE TABLE IF NOT EXISTS users (
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(100) NOT NULL,
-      email VARCHAR(100) UNIQUE NOT NULL,
-      created_at TIMESTAMP DEFAULT NOW()
+    CREATE TABLE IF NOT EXISTS user_positions (
+      id VARCHAR(100) NOT NULL,
+      latitude FLOAT,
+      longitude FLOAT
     );
   `;
 
     try {
         await pool.query(query);
-        console.log("✅ 'users' table created or already exists");
+        console.log("✅ 'user_positions' table created or already exists");
     } catch (err) {
         console.error("❌ Error creating 'users' table:", err);
     }
